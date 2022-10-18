0694c6b32baab8c4755c88115fed9932cf2ef239
brendanacelii <bac8@rice.edu>
2022-10-18 22:56:41 +0000

    progress on problem 2.e

--- graph_tools/epidemics_utils.py
+++ graph_tools/epidemics_utils.py
@@ -10,7 +10,9 @@ def simulate_infection_size(
     dt = 0.05,
     ensure_probability_between_0_1 = False,
     plot = True,
+    plot_pit = False,
     figsize = (6,6),
+    return_pit = False,
     ):
     """
     From homework 2 of networks class
@@ -25,7 +27,7 @@ def simulate_infection_size(
     pt.append(np.sum(pit))
     for i,t in enumerate(timesteps):
 
-        pit = pit + (B * (1 - pit) * (A@pit) - y*pit)*dt
+        pit = pit + (B * (1 - pit) * (A@pit) - (y*pit))*dt
         if ensure_probability_between_0_1:
             pit[pit > 1] = 1
             pit[pit < 0] = 0
@@ -33,21 +35,32 @@ def simulate_infection_size(
         pt_curr = np.sum(pit)
         pt.append(pt_curr)
 
+    parameters_str = (
+        fr"$\beta$ = {B}" + "\n" + 
+        fr"$\gamma$ = {y}"
+    )
     if plot:
         fig,ax = plt.subplots(1,1,figsize=figsize)
         ax.plot(np.concatenate([[0],timesteps]),pt)
         ax.set_xlabel("Time")
         ax.set_ylabel(f"Expected size of infection (Total Network Size = {len(A)})")
         ax.set_title(
-            "Simulated size of infection\n" +
-            fr"$\beta$ = {B}" + "\n" + 
-            fr"$\gamma$ = {y}"
+             "Simulated size of infection\n" + parameters_str
         )    
 #         ax.set_title(
 #             fr"Simulated size of infection\\n$\beta$ = {B}\\n$\gamma$ = {y}"
 #         )
         plt.show()
+    
+    if plot_pit:
+        fig,ax = plt.subplots(1,1,figsize=figsize)
+        ax.hist(pit,bins = 50)
+        ax.set_title(
+            f"Final Pi(t = {T}) histogam\n" + parameters_str
+        )
         
+    if return_pit:
+        return pt,pit
     return pt
 
 import epidemics_utils as epu
